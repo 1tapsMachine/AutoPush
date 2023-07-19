2c983797a0e0dcd4a62e3622b12679c68ab78fb9
Sami <samiztsouhailsami@gmail.com>
2023-07-19 12:51:43 +0100

    dn

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # This script is used to compile the project
 gcc -o AutoPush .files/cpshUpdated.c && echo "Compile success!" || echo "Compile failed!";
-sleep 2;
-clear;
+sleep 1;
+exit;
